9484a6176e23ade47cae8530fb02b5be89458a44
epcool <92027325+epcool@users.noreply.github.com>
2021-10-25 12:42:24 +0300

    img

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const main = async () => {
         console.log(`docker pull ${image}`)
         const regex = /[\/\:\.]/ig
 
-        const tmp = dockerFileName.split('/').pop()
+        const tmp = image.split('/').pop()
         const tarName =tmp.replace(regex,'_')      
         console.log(`docker save -o ${tarName}.tar ${image}`)
 
